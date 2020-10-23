3f4d7623c191fa94af6d5c5e24b0b9f59560a431
itexpert120 <itexpert120@gmail.com>
2020-10-23 06:40:40 -0400

    fortuna3g: shift some props to gprimelte-common

--- system_prop.mk
+++ system_prop.mk
@@ -2,11 +2,3 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qualcomm.cabl=0
 
-# LCD
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=240
-
-# RAM
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.config.low_ram=false \
-	ro.config.zram=false
