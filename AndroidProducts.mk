012f02b01eb272a5cf5baa33ecccb56291fe4c19
itexpert120 <itexpert120@gmail.com>
2020-10-24 09:13:08 -0400

    LineageOS => FusionOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := device/samsung/fortuna3g
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_fortuna3g.mk
+	$(LOCAL_DIR)/aosp_fortuna3g.mk
 
 COMMON_LUNCH_CHOICES := \
-	lineage_fortuna3g-eng \
-	lineage_fortuna3g-userdebug \
-	lineage_fortuna3g-user
+	aosp_fortuna3g-eng \
+	aosp_fortuna3g-userdebug \
+	aosp_fortuna3g-user
 
